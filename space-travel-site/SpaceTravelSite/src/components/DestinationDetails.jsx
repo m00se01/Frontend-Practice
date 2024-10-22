db1fc6c462ae3aa26bcccbac95c86239638533ce
Brandon <crandon1327@gmail.com>
2024-10-22 14:27:07 -0500

    finished desktop styling for destination page

--- space-travel-site/SpaceTravelSite/src/components/DestinationDetails.jsx
+++ space-travel-site/SpaceTravelSite/src/components/DestinationDetails.jsx
@@ -21,14 +21,14 @@ function DestinationDetails() {
   // console.log("destination page: ", destinations[0]);
 
   return (
-    <div className="destinations-wrapper">
+    <div className="destinations-wrapper ">
       <h1 className="uppercase ff-sans-cond letter-spacing-2 text-white fs-500 ">
         <span className="number-span letter-spacing-2">01</span> Pick your
         destination
       </h1>
 
-      <div className="destination-content">
-        <div className="destination-img ">
+      <div className="destination-content ">
+        <div className="destination-img  ">
           <source
             srcSet={destinations[currDestination].images.webp}
             type="image/webp"
@@ -39,49 +39,53 @@ function DestinationDetails() {
           />
         </div>
 
-        <ul className="destination-tabs uppercase fs-500 ff-sans-cond letter-spacing-1">
-          <li className={currDestination === 0 ? "active-tab" : ""}>
-            <button onClick={() => changeDestination("moon")}>Moon</button>
-          </li>
-          <li className={currDestination === 1 ? "active-tab" : ""}>
-            <button onClick={() => changeDestination("mars")}>Mars</button>
-          </li>
-          <li className={currDestination === 2 ? "active-tab" : ""}>
-            <button onClick={() => changeDestination("europa")}>Europa</button>
-          </li>
-          <li className={currDestination === 3 ? "active-tab" : ""}>
-            <button onClick={() => changeDestination("titan")}>Titan</button>
-          </li>
-        </ul>
+        <div className="border destination-info">
+          <ul className="destination-tabs uppercase fs-500 ff-sans-cond letter-spacing-1">
+            <li className={currDestination === 0 ? "active-tab" : ""}>
+              <button onClick={() => changeDestination("moon")}>Moon</button>
+            </li>
+            <li className={currDestination === 1 ? "active-tab" : ""}>
+              <button onClick={() => changeDestination("mars")}>Mars</button>
+            </li>
+            <li className={currDestination === 2 ? "active-tab" : ""}>
+              <button onClick={() => changeDestination("europa")}>
+                Europa
+              </button>
+            </li>
+            <li className={currDestination === 3 ? "active-tab" : ""}>
+              <button onClick={() => changeDestination("titan")}>Titan</button>
+            </li>
+          </ul>
 
-        <div className="destination-info">
-          <div className="planet-description">
-            <h2 className="fs-700 ff-serif uppercase">
-              {destinations[currDestination].name}
-            </h2>
-            <p className="fs-400 ff-sans-normal text-accent ">
-              {destinations[currDestination].description}
-            </p>
-          </div>
-
-          <div className="divider-line"></div>
-
-          <div className="planet-data">
-            <div>
-              <h4 className="ff-sans-cond letter-spacing-1 fs-300 uppercase text-accent">
-                Avg. Distance
-              </h4>
-              <p className="fs-500 ff-serif">
-                {destinations[currDestination].distance}
+          <div className="destination-details">
+            <div className="planet-description">
+              <h2 className="fs-700 ff-serif uppercase">
+                {destinations[currDestination].name}
+              </h2>
+              <p className="fs-400 ff-sans-normal text-accent ">
+                {destinations[currDestination].description}
               </p>
             </div>
-            <div>
-              <h4 className="ff-sans-cond letter-spacing-1 fs-300 uppercase text-accent">
-                Est. Travel Time
-              </h4>
-              <p className="fs-500 ff-serif uppercase ">
-                {destinations[currDestination].travel}
-              </p>
+
+            <div className="divider-line"></div>
+
+            <div className="planet-data">
+              <div>
+                <h4 className="ff-sans-cond letter-spacing-1 fs-300 uppercase text-accent">
+                  Avg. Distance
+                </h4>
+                <p className="fs-500 ff-serif">
+                  {destinations[currDestination].distance}
+                </p>
+              </div>
+              <div>
+                <h4 className="ff-sans-cond letter-spacing-1 fs-300 uppercase text-accent">
+                  Est. Travel Time
+                </h4>
+                <p className="fs-500 ff-serif uppercase ">
+                  {destinations[currDestination].travel}
+                </p>
+              </div>
             </div>
           </div>
         </div>
