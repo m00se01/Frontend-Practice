7c111678a824060fb0d02f25908ea2e7e2d7572f
Brandon <crandon1327@gmail.com>
2024-10-22 13:52:17 -0500

    styles for mobile and tablet screens for the destinations page

--- space-travel-site/SpaceTravelSite/src/pages/DestinationPage.jsx
+++ space-travel-site/SpaceTravelSite/src/pages/DestinationPage.jsx
@@ -9,16 +9,7 @@ export const DestinationPage = () => {
         <Navbar currActive={"destination"} />
       </header>
 
-      <div className="border content-wrapper ">
-        <h1 className="uppercase ff-sans-cond letter-spacing-2 text-white ">
-          <span className="number-span letter-spacing-2">01</span> Pick your
-          destination
-        </h1>
-
-        <div className="destination-content-container">
-          <DestinationDetails />
-        </div>
-      </div>
+      <DestinationDetails />
     </div>
   );
 };
