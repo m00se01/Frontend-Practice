bf6620631bc179476591d6173fad7d92d4456683
Brandon <crandon1327@gmail.com>
2024-10-25 15:58:40 -0500

    technology tablet styles and starting on desktop styles

--- space-travel-site/SpaceTravelSite/src/components/TechDetails.jsx
+++ space-travel-site/SpaceTravelSite/src/components/TechDetails.jsx
@@ -1,61 +1,86 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/tech.css";
 import { technology } from "../data.json";
 
 export const TechDetails = () => {
   const [currTech, setCurrTech] = useState(0);
+  const [isLandscape, setIsLandscape] = useState(window.innerWidth < 1280);
 
   const changeTech = (tech) => {
     setCurrTech(tech);
   };
 
-  console.log(window.screen.width);
+  useEffect(() => {
+    const handleResize = () => setIsLandscape(window.innerWidth < 1280);
+    console.log(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="tech-details-container">
-      <header>
-        <h1 className="uppercase ff-sans-cond letter-spacing-2 text-white fs-400 ">
-          <span className="number-span letter-spacing-2">03</span>
-          Space Launch 101
-        </h1>
-      </header>
+      <h1 className="page-heading uppercase ff-sans-cond letter-spacing-2 text-white fs-400 ">
+        <span className="number-span letter-spacing-2">03</span>
+        Space Launch 101
+      </h1>
+
+      <div
+        className="tech-img "
+        // style={{
+        //   backgroundImage: `url(${
+        //     isLandscape
+        //       ? technology[currTech].images.landscape
+        //       : technology[currTech].images.portrait
+        //   })`,
+        // }}
+      >
+        <img
+          className=""
+          src={
+            isLandscape
+              ? technology[currTech].images.landscape
+              : technology[currTech].images.portrait
+          }
+          alt=""
+        />
+      </div>
 
       <div className=" tech-content-wrapper ">
-        <div
-          className="tech-img landscape"
-          style={{
-            backgroundImage: `url${technology[currTech].images.landscape}`,
-          }}
-        >
-          <img
-            className="landscape"
-            src={technology[currTech].images.landscape}
-            alt=""
-          />
-        </div>
-
-        <div className="tech-img portrait">
-          <img
-            className="landscape"
-            src={technology[currTech].images.portrait}
-            alt=""
-          />
-        </div>
+        {/* Image */}
 
         <div>
+          {/* Buttons */}
           <div className="tech-btns">
-            <button onClick={() => changeTech(0)}>1</button>
-            <button onClick={() => changeTech(1)}>2</button>
-            <button onClick={() => changeTech(2)}>3</button>
+            <button
+              className={currTech == 0 ? "active-tech" : ""}
+              onClick={() => changeTech(0)}
+            >
+              1
+            </button>
+            <button
+              className={currTech == 1 ? "active-tech" : ""}
+              onClick={() => changeTech(1)}
+            >
+              2
+            </button>
+            <button
+              className={currTech == 2 ? "active-tech" : ""}
+              onClick={() => changeTech(2)}
+            >
+              3
+            </button>
           </div>
 
           <div className="tech-description">
-            <h2 className="fs-400 text-white ff-sans-normal">
-              <span className="d-block fs-500 text-accent ff-serif uppercase">
+            <h2 className="fs-600 text-white ff-serif uppercase">
+              <span className="d-block fs-400 text-accent ff-serif uppercase">
                 The Terminology...
               </span>
-              {technology[currTech].description}
+              {technology[currTech].name}
             </h2>
+            <p className="text-accent ff-sans-cond fs-400">
+              {technology[currTech].description}
+            </p>
           </div>
         </div>
       </div>
